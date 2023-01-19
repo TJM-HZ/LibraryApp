89a7a7e2dd81fd8f920b40bea00934251bfea2bf
TJM-HZ <mein0009@hz.nl>
2023-01-19 11:24:41 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
