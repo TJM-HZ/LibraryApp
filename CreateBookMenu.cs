d89f94a1ecb14946939ecf558db2e938c91b74b4
TJM-HZ <mein0009@hz.nl>
2023-01-21 08:14:09 +0100

    Book creation menu almost finished

---
+++ CreateBookMenu.cs
@@ -0,0 +1,35 @@
+﻿using LibraryApp.Products.ProductBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    internal class CreateBookMenu
+    {
+        public CreateBookMenu(BookBuilder bb)
+        {
+            Console.Clear();
+            Console.WriteLine("Creating a new book");
+            Console.WriteLine("Fields marked with * are required");
+            RequiredStringField("Title", bb, bb.Title);
+            RequiredStringField("Title", bb, bb.Author);
+        }
+        public void RequiredStringField(string fieldName, BookBuilder bb, Func<string, BookBuilder> method)
+        {
+            Console.Write("Title*: ");
+            string input = Console.ReadLine();
+            if (input == null || input == "")
+            {
+                Console.Clear();
+                RequiredStringField(fieldName, bb, method);
+            }
+            else
+            {
+                bb.Title(input);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,25 +10,8 @@ namespace LibraryApp
         static void Main(string[] args)
         {
             BookBuilder bb = BookBuilder.GetInstance();
+            CreateBookMenu cbMenu = new CreateBookMenu(bb);
 
-            Book book1 = bb
-                .SetTitle("funbook")
-                .SetAuthor("A funny person")
-                .Build();
-            Book book2 = bb
-                .SetTitle("booker2")
-                .SetAuthor("A writer of books")
-                .Build();
-
-            ProductBundle bundle1 = new ProductBundle("Big Bundle");
-            bundle1.Add(book1);
-            bundle1.Add(book2);
-
-            bundle1.PrintDetails();
-
-            bundle1.Remove(book1);
-
-            bundle1.PrintDetails();
         }
     }
 }
